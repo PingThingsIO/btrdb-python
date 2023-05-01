59f9e3aa36707b9bc6e1bee639a9acb8ce2a1125
Justin Gilmer <justin@pingthings.io>
2023-05-01 17:55:17 -0500

    Add logging and initial attempt to decode ipc vals
    Reads the arrowbytes and attempts to decode the stream using the arrow schema we get from the open_stream call from pyarrow.

--- btrdb/experimental/stream.py
+++ btrdb/experimental/stream.py
@@ -1,32 +1,51 @@
-import btrdb
-from btrdb.stream import StreamSet, Stream
+import logging
+
 import pyarrow as pa
+
+import btrdb
+from btrdb.stream import Stream, StreamSet
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
 
 
 class ArrowStream(Stream):
     """Arrow-accelerated queries where applicable for a single stream."""
-    def __init__(self, btrdb:btrdb.BTrDB, uuid:str, db_values:dict = None):
+
+    def __init__(self, btrdb: btrdb.BTrDB, uuid: str, db_values: dict = None):
         super().__init__(self, btrdb=btrdb, uuid=uuid, **db_values)
 
     @classmethod
-    def from_stream(cls, stream:btrdb.stream.Stream):
+    def from_stream(cls, stream: btrdb.stream.Stream):
         return cls(btrdb=stream.btrdb, uuid=stream.uuid, db_values=None)
+
 
 class ArrowStreamSet(StreamSet):
     """Arrow-accelerated queries where applicable for a set of streams."""
+
     def __init__(self, streams):
         super().__init__(streams=streams)
 
     @classmethod
-    def from_streamset(cls, streamset:btrdb.stream.StreamSet):
+    def from_streamset(cls, streamset: btrdb.stream.StreamSet):
         return cls(streams=streamset._streams)
 
-# TODO: how to decode the arrow bytes?
-    def values(self, start:int, end:int):
+    # TODO: how to decode the arrow bytes?
+    # TODO: need to ensure we exhaust the generator from the endpoint
+    def values(self, start: int, end: int):
         """Return a numpy array from arrow bytes"""
-        arrow_vals = []
+        list_of_data = []
+        logger.debug("In values method for ArrowStreamSet")
         for s in self._streams:
-            arrow_vals.append(s._btrdb.ep.arrowRawValues(uu=s.uu.bytes, start=start, end=end, version=0))
-        arr = pa.Array().from_buffers(buffers=arrow_vals, length=len(arrow_vals[0]), DataType_type=None)
-        return arr.to_numpy()
-
+            logger.debug(f"For stream - {s.uuid} -  {s.name}")
+            arr_bytes, ver = s._btrdb.ep.arrowRawValues(
+                uu=s.uu.bytes, start=start, end=end, version=0
+            )
+            with pa.ipc.open_stream(arr_bytes) as reader:
+                schema = reader.schema
+                metadata = reader.metadata
+                logger.debug(f"schema: {schema}")
+                logger.debug(f"metadata: {metadata}")
+                batches = [b for b in reader]
+            list_of_data.append(pa.table(data=batches, schema=schema))
+        return list_of_data
