108a95d4e5ee6bfe8d72a9ecdccc51ddc345e2dd
Andrew Chambers <ac@acha.ninja>
2023-05-18 20:22:28 +1200

    more streams

--- d.py
+++ d.py
@@ -6,11 +6,11 @@ import time
 
 
 if __name__ == '__main__':
-	cluster = dask.distributed.LocalCluster(n_workers=16,threads_per_worker=1)
+	cluster = dask.distributed.LocalCluster(n_workers=32,threads_per_worker=1)
 	client = dask.distributed.Client(cluster)
 	btrdb.utils.dask.configure_cluster(client)
 	conn = btrdb.connect()
-	streams = conn.streams_in_collection("andy")[:5]
+	streams = conn.streams_in_collection("andy")[:10]
 	start = streams[0].earliest()[0].time
 	end = start + 14*24*60*60*(10**9) # streams[0].latest()[0].time-1
 	t1=time.time()
