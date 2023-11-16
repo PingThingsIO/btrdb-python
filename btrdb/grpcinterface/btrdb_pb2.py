7199a3e262f865e1401d38ec1079e8cb10b6cf4d
Justin Gilmer <justin@pingthings.io>
2023-11-16 14:53:50 -0500

    remove bytewax connector for the time being.

--- btrdb/grpcinterface/btrdb_pb2.py
+++ btrdb/grpcinterface/btrdb_pb2.py
@@ -19,7 +19,6 @@ _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
 _builder.BuildTopDescriptorsAndMessages(DESCRIPTOR, 'btrdb_pb2', _globals)
 if _descriptor._USE_C_DESCRIPTORS == False:
-
   DESCRIPTOR._options = None
   _globals['_MERGEPOLICY']._serialized_start=6465
   _globals['_MERGEPOLICY']._serialized_end=6525
