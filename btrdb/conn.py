f54e5f2b7f2cf82903233ec7f0ea9d6ac05ab37f
Jeff Lin <42981468+jleifnf@users.noreply.github.com>
2023-11-27 18:22:12 -0800

    FutureWarning for streams_in_collection to return StreamSet (#60)
    Co-authored-by: Justin Gilmer <justin@pingthings.io>

--- btrdb/conn.py
+++ btrdb/conn.py
@@ -22,6 +22,7 @@ import re
 import uuid as uuidlib
 from concurrent.futures import ThreadPoolExecutor
 from typing import List
+from warnings import warn
 
 import certifi
 import grpc
@@ -578,7 +579,12 @@ class BTrDB(object):
                             property_version=desc.propertyVersion,
                         )
                     )
-
+        # TODO: In future release update this method to return a streamset object.
+        warn(
+            "StreamSet will be returned in a future release.",
+            FutureWarning,
+            stacklevel=2,
+        )
         return result
 
     @retry
